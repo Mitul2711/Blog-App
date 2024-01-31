b08ff3d1f252d27464e33b883ca90479645b0d30
Mitul <mitulgondaliya27@gmail.com>
2024-01-31 11:07:29 +0530

    add firebase

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    fireBaseConfig: {
+        apiKey: "AIzaSyCUgHN2nTf7iJTtNltg5JR6KRFGuSmmbY8",
+        authDomain: "ang-blog-d0c9b.firebaseapp.com",
+        projectId: "ang-blog-d0c9b",
+        storageBucket: "ang-blog-d0c9b.appspot.com",
+        messagingSenderId: "370596205824",
+        appId: "1:370596205824:web:a1029b9ae9c99b6ffa4fe5",
+        measurementId: "G-5RSY3Y6E93"
+    }
+}
