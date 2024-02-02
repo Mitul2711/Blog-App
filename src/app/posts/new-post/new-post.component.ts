dcbf1b2c53fe3e0cdb6c1fa699c7c129bdd32bba
Mitul <mitulgondaliya27@gmail.com>
2024-02-02 15:18:43 +0530

    create post interface

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Post } from 'src/app/models/post';
 import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
@@ -56,6 +57,21 @@ export class NewPostComponent implements OnInit {
 
   onSubmit() {
     console.log(this.postForm.value )
+    const postData: Post = {
+        title: this.postForm.value.title,
+        permalink: this.postForm.value.permalink,
+        category: {
+          categoryId: '',
+          category: ''
+        },
+        postImgPath: '',
+        excerpt: this.postForm.value.excerpt,
+        content: this.postForm.value.content,
+        isFeatured: false,
+        views: 0,
+        status: 'new',
+        createdAt: new Date()
+    }
   }
  
 }
